a2e7a8cb2f41b2115193bb6f663ebd4501df0f42
srichardyorke <srichardyorke@C218-1762.yccc.edu>
2018-04-18 19:52:48 -0400

    Update of project 3

--- src/main/java/Test/project3.java
+++ src/main/java/Test/project3.java
@@ -1,9 +1,11 @@
 package Test;
 
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPasswordField;
+
+import edu.yccc.java.samples.slack.SlackService;
+
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -44,16 +46,19 @@ public class project3 {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		pwdPassword = new JPasswordField();
 		pwdPassword.setText("Password");
 		pwdPassword.setBounds(284, 213, 113, 20);
 		frame.getContentPane().add(pwdPassword);
-		
+
 		JButton btnEnter = new JButton("Enter");
 		btnEnter.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
+				 
+				System.out.println("Call Slack Service!");
+				SlackService slackService = new SlackService();
+				slackService.sendMessage("#richard", "RCY383", "Hello!");
 			}
 		});
 		btnEnter.setBounds(206, 144, 89, 23);
