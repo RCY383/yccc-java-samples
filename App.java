7fbb4dbd58a4cf9fb5f5d940ca4e918bf4566aed
Richard Yorke <srichardyorke@yccc.edu>
2018-04-18 07:33:46 -0400

    The two questions have been answered.

---
+++ App.java
@@ -0,0 +1,14 @@
+package slackIntegration;
+
+// The following questions are part of the third project
+
+// 1. What is Maven? Maven is a tool that can build the Ant files easier. It can also make the files easier to manage within Java. 
+// When you know how to use Meven correctly, then you can save a lot of time.
+
+// 2. What is a jar file? A jar file is a packaged file format that is part of Java Development Kit. 
+// You can use it to for achieving, decompression, unpacking, and the prevention of Loss data within in the classes.
+
+
+public class App {
+	
+}
